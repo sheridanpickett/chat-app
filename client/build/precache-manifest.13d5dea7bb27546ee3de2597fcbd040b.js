a158ce5e4230fabbb39da953fc109c8bda05d646
sheridan <sheridan.pickett@hotmail.co.uk>
2019-06-24 09:26:53 +0100

    Added db password

---
+++ client/build/precache-manifest.13d5dea7bb27546ee3de2597fcbd040b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db78b2ca2c1b34716094422d78fab10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90b50abcbefa9926cd9b",
+    "url": "/static/js/2.198bd18a.chunk.js"
+  },
+  {
+    "revision": "37b9c4390e15dedd2a51",
+    "url": "/static/js/main.44b6c205.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const path = require('path')
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {pingTimeout: 20000});
 const mongoose = require('mongoose');
@@ -10,6 +11,7 @@ const connectDB = require('./config/connect');
 
 connectDB();
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(express.json());
 
 app.post('/createUser', async (req, res) => {
